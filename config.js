6c3765107e2d2492472910cc4914cc03d89c84d1
Eóin Martin <eoinmartin3007@gmail.com>
2014-07-16 21:12:13 +0100

    Add Kickass to providers

--- config.js
+++ config.js
@@ -1,19 +1,8 @@
 module.exports = {
-	master: false,
+	master: true,
 	port: 5000,
 	workers: 2,
 	scrapeTime: '00 00 3,15 * * *',
 	pageSize: 50,
-	dbHosts: [
-		'fr.ptnet',
-		'us-chi.ptnet',
-		'us-mia.ptnet',
-		'us-dal.ptnet',
-		'us-sjc.ptnet',
-		'za.ptnet',
-		'br.ptnet',
-		'sg.ptnet',
-		'uk.ptnet',
-		'ca.ptnet'
-	]
+	dbHosts: ['localhost']
 }
